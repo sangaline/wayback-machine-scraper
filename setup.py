da4fc2a13d7c7edce57703aac1c79d11b70f9819
Raphael Merx <raphael.merx@gmail.com>
2023-04-30 12:39:20 +0800

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = description + \
 
 setup(
     name='wayback-machine-scraper',
-    version='1.0.7',
+    version='1.1.0',
     author='Evan Sangaline',
     author_email='evan@intoli.com',
     description=description,
